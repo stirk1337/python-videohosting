4577d9cb76644d2bf71a02f2bb97be12f5d06602
Ramil <stirk2281337@gmail.com>
2026-02-28 14:17:13 +0500

    test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
-import os
-import sys
-import json
-from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 from app.services import UserService, VideoService
